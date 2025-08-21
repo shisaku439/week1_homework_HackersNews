81d9a46c3a155b70eea6a7105e57dfb85ba34484
shisaku439 <shisaku555@gmail.com>
2025-08-21 17:35:11 +0900

    refactoring

--- HackersNews.py
+++ HackersNews.py
@@ -2,36 +2,59 @@ import time
 import requests
 
 
-# apiのURLと取得記事数
+# apiのURL
 TOPSTORIES_URL = "https://hacker-news.firebaseio.com/v0/topstories.json"
-ITEM_URL = " https://hacker-news.firebaseio.com/v0/item/"
-MAX_ITEM = 30
+ITEM_URL = "https://hacker-news.firebaseio.com/v0/item/"
 
 
-# トップページ記事の情報取得
-def get_top_story_id_list():
-    return requests.get(TOPSTORIES_URL).json()
+def get_top_story_id_list(max_item=500):
+    """トップページ記事の情報取得
+    Args:
+        max_item(int): 取得する記事の数（最大500）
+    Returns:
+        list:トップページに表示されるidのリスト
+    """
+
+    res = requests.get(TOPSTORIES_URL).json()
+    return res[0:max_item]
 
 
-# 特定idの記事情報の取得
 def get_story_dict(id):
+    """特定idの記事情報の取得
+    Args:
+        id(int):取得する記事のid
+    Returns:
+        dict:記事情報のdict
+    """
+
     url = f"{ITEM_URL}{id}.json"
     return requests.get(url).json()
 
 
+def formated_dict(dict):
+    """特定idの記事情報の取得
+    Args:
+        id(dict):記事情報のdict
+    Returns:
+        dict:titleとlinkの辞書型
+    """
+
+    return {"title": dict["title"], "link": dict.get("url") or "None"}
+
+
 def main():
     # 記事情報
-    topstories_list = get_top_story_id_list()
-    top_thirty_list = topstories_list[0:MAX_ITEM]
+    top_thirty_list = get_top_story_id_list(max_item=30)
 
     time.sleep(1)  # ここで1秒止まる
 
     # 記事の情報取得
     for id in top_thirty_list:
         item = get_story_dict(id)
-        result = {"title": item["title"], "link": item.get("url") or "None"}
+        result = formated_dict(item)
         print(result)
         time.sleep(1)  # ここで1秒止まる
 
 
-main()
+if __name__ == "__main__":
+    main()
